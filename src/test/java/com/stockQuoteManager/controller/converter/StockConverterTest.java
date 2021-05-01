a2a76bace6b3e48603815d9abfdd5cc7b0a43a93
daniellygj <daniellygj@gmail.com>
2021-05-01 15:44:59 -0300

    teste converter

--- src/test/java/com/stockQuoteManager/controller/converter/StockConverterTest.java
+++ src/test/java/com/stockQuoteManager/controller/converter/StockConverterTest.java
@@ -11,7 +11,9 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -53,6 +55,56 @@ public class StockConverterTest {
         assertEquals(stockDTO.getId(), stock.getStockName());
         assertEquals(stockDTO.getQuotes().entrySet().iterator().next().getKey(), stock.getQuotes().get(0).getDate().toString());
         assertEquals(stockDTO.getQuotes().entrySet().iterator().next().getValue(), stock.getQuotes().get(0).getValue());
+    }
+
+    @Test
+    public void shouldConvertStockListToDTO() {
+        Quote quote1 = QuoteTestBuilder
+                .init()
+                .withDefaultValues()
+                .build();
+
+        Stock stock1 = StockTestBuilder
+                .init()
+                .withDefaultValues()
+                .quotes(Collections.singletonList(quote1))
+                .build();
+
+        Quote quote2 = QuoteTestBuilder
+                .init()
+                .withDefaultValues()
+                .build();
+
+        Stock stock2 = StockTestBuilder
+                .init()
+                .withDefaultValues()
+                .quotes(Collections.singletonList(quote2))
+                .build();
+
+        Quote quote3 = QuoteTestBuilder
+                .init()
+                .withDefaultValues()
+                .build();
+
+        Stock stock3 = StockTestBuilder
+                .init()
+                .withDefaultValues()
+                .quotes(Collections.singletonList(quote3))
+                .build();
+
+        List<StockDTO> stockDTO = converter.stockListToDTO(Arrays.asList(stock1, stock2, stock3));
+
+        assertEquals(stock1.getStockName(), stockDTO.get(0).getId());
+        assertEquals(stock1.getQuotes().get(0).getDate().toString(), stockDTO.get(0).getQuotes().entrySet().iterator().next().getKey());
+        assertEquals(stock1.getQuotes().get(0).getValue(), stockDTO.get(0).getQuotes().entrySet().iterator().next().getValue());
+
+        assertEquals(stock1.getStockName(), stockDTO.get(1).getId());
+        assertEquals(stock1.getQuotes().get(0).getDate().toString(), stockDTO.get(1).getQuotes().entrySet().iterator().next().getKey());
+        assertEquals(stock1.getQuotes().get(0).getValue(), stockDTO.get(1).getQuotes().entrySet().iterator().next().getValue());
+
+        assertEquals(stock1.getStockName(), stockDTO.get(2).getId());
+        assertEquals(stock1.getQuotes().get(0).getDate().toString(), stockDTO.get(2).getQuotes().entrySet().iterator().next().getKey());
+        assertEquals(stock1.getQuotes().get(0).getValue(), stockDTO.get(2).getQuotes().entrySet().iterator().next().getValue());
     }
 
 }
