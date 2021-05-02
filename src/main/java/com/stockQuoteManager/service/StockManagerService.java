2d2ef8dcc611ca3270bc251102b7efa787b27a8d
daniellygj <daniellygj@gmail.com>
2021-05-02 13:48:15 -0300

    configuração docker-compose

--- src/main/java/com/stockQuoteManager/service/StockManagerService.java
+++ src/main/java/com/stockQuoteManager/service/StockManagerService.java
@@ -12,6 +12,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import static java.lang.Thread.sleep;
+
 @Service
 public class StockManagerService {
 
@@ -20,9 +22,15 @@ public class StockManagerService {
 
     public void getStockList() {
         try {
-            String url = "http://localhost:8080/stock";
-            HttpResponse<String> response = null;
-            response = getRequest(url);
+            String stockManagerUrl = System.getenv("STOCK_MANAGER_URL");
+            String url;
+            if (stockManagerUrl == null) {
+                url = "http://localhost:8080/notification";
+            } else {
+                url = "http://" + stockManagerUrl + ":8080/notification";
+            }
+
+            HttpResponse<String> response = getRequest(url);
             cache.add("stock", response.body());
 
         } catch (IOException | InterruptedException e) {
@@ -55,21 +63,39 @@ public class StockManagerService {
     }
 
     @PostConstruct
-    private void register() throws IOException, InterruptedException {
+    private void register() throws InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
-        String url = "http://localhost:8080/notification";
+        String stockManager = System.getenv("STOCK_MANAGER_URL");
+        String stockManagerUrl;
+        if (stockManager == null) {
+            stockManagerUrl = "http://localhost:8080/notification";
+        } else {
+            stockManagerUrl = "http://" + stockManager + ":8080/notification";
+        }
 
-        String body = "{\n" +
-                " \"host\": \"localhost\",\n" +
-                " \"port\": 8081\n" +
-                "} ";
+        String stockQuoteManager = System.getenv("STOCK_MANAGER_SERVICE");
+
+        String body;
+        if(stockQuoteManager == null) {
+            body = "{ \"host\": \"localhost\", \"port\": 8081 } ";
+        } else {
+            body = String.format("{ \"host\": \"%s\", \"port\": 8081  }", stockQuoteManager);
+        }
 
         HttpRequest request = HttpRequest.newBuilder(
-                URI.create(url))
+                URI.create(stockManagerUrl))
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(body))
                 .build();
 
-        client.send(request, HttpResponse.BodyHandlers.ofString());
+        while (true) {
+            try {
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+                break;
+            } catch (IOException e) {
+                sleep(4000);
+                System.out.println("Trying to send request to stock-manager");
+            }
+        }
     }
 }
